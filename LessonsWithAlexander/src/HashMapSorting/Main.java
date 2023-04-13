0477ac864d698915abe8b06f3f1048231fc0bf82
Tonkih <alenatonkich17@gmail.com>
2023-04-13 17:38:36 +0300

    HashMap bubble sort

---
+++ LessonsWithAlexander/src/HashMapSorting/Main.java
@@ -0,0 +1,8 @@
+package HashMapSorting;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Sorting.createHashMap());
+        Sorting.hashMapSorting(Sorting.createHashMap());
+    }
+}
